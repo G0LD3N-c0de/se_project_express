cfb4967002b7f96c40cbd2c5352aeabb5d97af05
SpaceMan28 <nicokazemi34@gmail.com>
2023-09-30 11:33:59 -0600

    add models

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const { PORT = 3001 } = process.env;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`App is listening to ${PORT}`);
